82d8595554a83bd7a109c3cf9ace3c3da9189148
Mahesh V N <work@Maheshs-MacBook-Pro.local>
2021-12-28 12:59:57 +0530

    cors and body-parser middleware added

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const importedData = require('./data.json')
+const cors = require('cors')
+const bodyParser = require('body-parser')
 
 const app = express()
-const PORT = process.env.PORT || 3000
+
+app.use(cors())
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     console.log('Hello Shri Krishna')
@@ -13,6 +17,7 @@ app.get('/achyutashtakam', (req, res) =>{
     res.send(importedData);
 } )
 
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log(`server is running on port: ${PORT}`)
 })
